eebfd125dc31251b8702722d46a8a73d5a58c2d5
LucasC78 <lucascoutantpro@gmail.com>
2023-04-19 00:05:56 +0200

    push 19 avril

--- m2l/src/DeleteArticles.js
+++ m2l/src/DeleteArticles.js
@@ -19,7 +19,7 @@ export default function SuppressionArticles() {
                 console.log(res)
                 if (res.status === 200) {
                     alert("Suppression réussi")
-                    navigate("/Adminmodify");
+                    navigate("/ModifyArticles");
                 }
                 else {
                     alert("Erreur de supression")
